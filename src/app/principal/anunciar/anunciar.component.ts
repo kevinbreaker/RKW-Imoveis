100d87dc5ba6f2fd279c9ef68a363247480b349d
Walteann Costa <walteann.costa.silva@accenture.com>
2018-10-24 18:47:56 -0300

    minha localizacao

--- src/app/principal/anunciar/anunciar.component.ts
+++ src/app/principal/anunciar/anunciar.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MAT_STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 import { MouseEvent, InfoWindowManager } from '@agm/core';
-
 @Component({
     selector: 'app-anunciar',
     templateUrl: './anunciar.component.html',
@@ -15,6 +14,9 @@ export class AnunciarComponent implements OnInit {
 
     firstFormGroup: FormGroup;
     secondFormGroup: FormGroup;
+
+    suaLatitude = null;
+    suaLogintude = null;
 
     latitude = null;
     longitude = null;
@@ -30,6 +32,8 @@ export class AnunciarComponent implements OnInit {
         this.secondFormGroup = this._formBuilder.group({
             secondCtrl: ['', Validators.required]
         });
+
+        this.teste();
     }
 
     mapaClicado($event: MouseEvent) {
@@ -37,6 +41,19 @@ export class AnunciarComponent implements OnInit {
         this.longitude = $event.coords.lng;
         console.log($event.coords.lat);
         console.log($event.coords.lng);
+    }
+
+    teste() {
+        if (navigator.geolocation) {
+
+            navigator.geolocation.getCurrentPosition((posicao) =>  {
+                this.latitude = posicao.coords.latitude;
+                this.longitude = posicao.coords.longitude;
+                this.suaLatitude = posicao.coords.latitude;
+                this.suaLogintude = posicao.coords.longitude;
+            });
+
+        }
     }
 
 }
